8f6469ac2ef396f8e2bd4359d0f618d04e5a8ea3
iBogdanov <ilya.bogdanov14@gmail.com>
2016-10-12 23:21:34 +0300

    Добавлен скрипт для вычисления самого большого, самого маленького и самого близкого баров

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # 3_bars
+## Описание
+По данным из json-файла скрипт рассчитывает:
+
+* самый большой бар;
+* самый маленький бар;
+* самый близкий бар
+
+## Использование
+Скрипт принимает на вход:
+
+* путь до json-файла;
+* долготу;
+* широту
+
+```sh
+$ python3.5 ./bars.py ./bars.json 37.685499 55.765914
+```
+
+Указываются либо все перечисленные параметры, либо не указывается ни один дополнительный параметр. При некорректном составе параметров выводится сообщение об ошибке:
+```sh
+Error: incorrect parameters are transferred.
+It is necessary to transfer a file name, longitude and latitude,
+or not to transfer anything.
+```
+## Пример
+```sh
+$ python3.5 ./bars.py ./bars.json 37.685499 55.765914
+Smallest bar (seats count): БАР. СОКИ (0)
+Biggest bar (seats count): Ночной клуб «Орфей» (1000):
+Closest bar:  Хилл Фиш
+```
--- bars.py
+++ bars.py
@@ -1,21 +1,63 @@
+import sys
+import math
 import json
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
 
 
 def get_biggest_bar(data):
-    pass
+    biggest_bar = max(data, key = lambda bar: bar['Cells']['SeatsCount'])    
+    return (biggest_bar['Cells']['Name'], biggest_bar['Cells']['SeatsCount'])
 
 
 def get_smallest_bar(data):
-    pass
+    smallest_bar = min(data, key = lambda bar: bar['Cells']['SeatsCount'])    
+    return (smallest_bar['Cells']['Name'], smallest_bar['Cells']['SeatsCount'])
 
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    distance_to_current_bar = lambda bar: distance(
+        longitude,
+        latitude,
+        bar['Cells']['geoData']['coordinates'][0],
+        bar['Cells']['geoData']['coordinates'][1]
+    )
+    
+    closest_bar = min(data, key = distance_to_current_bar)
+    
+    return closest_bar['Cells']['Name']
 
+
+def distance(
+        my_longitude, my_latitude,
+        bar_longitude, bar_latitude):
+    return math.sqrt(
+        (my_longitude - bar_longitude) ** 2 +
+        (my_latitude - bar_latitude) ** 2
+    )
 
 if __name__ == '__main__':
-    pass
+    if len (sys.argv) == 1:
+        filepath = input('Enter file path > ')
+        longitude = float(input('Enter longitude > '))
+        latitude = float(input('Enter latitud > '))
+    elif len (sys.argv) != 4:
+        print (
+            'Error: incorrect parameters are transferred.\n'
+            'It is necessary to transfer a file name, longitude and latitude, '
+            'or not to transfer anything.'
+        )
+        sys.exit (1)
+    else:
+        filepath = sys.argv[1]
+        longitude = float(sys.argv[2])
+        latitude = float(sys.argv[3])
+    
+    data = load_data(filepath)
+
+    print('Smallest bar (seats count): %s (%d)' % get_smallest_bar(data))    
+    print('Biggest bar (seats count): %s (%d):' % get_biggest_bar(data))
+    print('Closest bar: ', get_closest_bar(data, longitude, latitude))  
